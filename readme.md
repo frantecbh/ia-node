57edb6d7d56d123a5c4f9398f24280be8912ec0d
FRANCISCO MENEZES <franciscomenezesbh@gmail.com>
2025-05-16 13:56:05 -0300

    conectando a openai

--- readme.md
+++ readme.md
@@ -1,9 +1,12 @@
 # Instalações
 
-npm i -D typescript tsx
-npm i -D @types/node
-npm i dotenv
-npm i -D eslint @rocketseat/eslint-config
-{
+- npm i -D typescript tsx
+- npm i -D @types/node
+- npm i dotenv
+- npm i -D eslint @rocketseat/eslint-config
+
+ - {
   "extends": "@rocketseat/eslint-config/node"
 }
+
+- npm i openai
--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,28 @@
 import dotenv from 'dotenv'
+import OpenAI from 'openai'
 dotenv.config()
 
-console.log('API Key', process.env.OPEN_IA_API_KEY)
+const client = new OpenAI({
+  apiKey: process.env.OPEN_IA_API_KEY,
+})
+
+client.chat.completions
+  .create({
+    model: 'gpt-4o-mini-2024-07-18',
+    messages: [
+      {
+        role: 'user',
+        content: 'Escreva uma frase sobre automações n8n',
+      },
+    ],
+  })
+  .then((completions) => {
+    console.log(completions.choices[0].message.content)
+  })
+
+// const response = await client.responses.create({
+//   model: 'gpt-4.1',
+//   input: 'Write a one-sentence bedtime story about a unicorn.',
+// })
+
+// console.log('API Key', process.env.OPEN_IA_API_KEY)
